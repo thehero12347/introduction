2021002166db41630ade59bd7e72700e0e326efb
thehero12347 <82715804+thehero12347@users.noreply.github.com>
2022-06-19 11:50:11 +0000

    added github link at homepage

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("discordJoin").onclick = function (discord) {
         location.href = "https://discord.gg/ZJNV7X7dWw"; 
     };
+
+document.getElementById("github").onclick = function (github) {
+location.href = "https://github.com/thehero12347";
+};
