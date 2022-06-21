61ed4615bc8beeb5a553d6fe1e676162990612e9
thehero12347 <82715804+thehero12347@users.noreply.github.com>
2022-06-21 10:11:38 +0000

    might change top bar so last snapshit i mean snapshot ig

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ document.getElementById("home").onclick = function (home) {
 document.getElementById("aboutus").onclick = function (aboutus) {
   location.href = "aboutus.html";
 };
+
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
